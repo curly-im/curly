4a7e577295850d09bf952fe7651440e5dd4c118f
michalpiatkowski <michal5piatkowski@gmail.com>
2018-08-07 18:47:35 +0200

    Added support for emojis

--- frontend/src/components/MessageInput.js
+++ frontend/src/components/MessageInput.js
@@ -2,6 +2,31 @@ import React from 'react';
 
 import '../styles/MessageInput.css';
 
+function changeSmile(str) {
+  if (str === ":)") {
+    return '🙂';
+  }
+  if (str === ":D") {
+    return '😁';
+  }
+  if (str === ";)") {
+    return '😉'
+  }
+  if (str === ":|") {
+    return '😐';
+  }
+  if (str === ":P") {
+    return '😛';
+  }
+  if (str === "8-)") {
+    return '😎';
+  }
+  if (str === ":(") {
+    return '😞';
+  }
+  return str;
+}
+
 export default function MessageInput({ onSendMessage }) {
     let inputElement = null;
     const onFormSubmit = (e) => {
@@ -11,10 +36,14 @@ export default function MessageInput({ onSendMessage }) {
         inputElement.value = '';
     };
 
+    function handleChange() {
+       inputElement.value = changeSmile(inputElement.value); 
+    };
+
     return (
         <div className="message-input">
           <form onSubmit={onFormSubmit}>
-            <input type="text" ref={el => inputElement = el}/>
+            <input onChange={handleChange} type="text" ref={el => inputElement = el}/>
             <button>
               Send
             </button>
