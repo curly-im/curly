0f7c8d33816007cf315fddb73d4aab47333a288b
Dawid Szlachta <dawids@telestream.net>
2018-08-07 19:07:02 +0200

    Added empty test for MessageInput

---
+++ frontend/src/components/MessageInput.test.js
@@ -0,0 +1,9 @@
+import { changeSmile } from './MessageInput';
+
+test('Message Input', () => {
+    it('should support Emoji', () => {
+        const rawString = ':)';
+
+        // TODO: implement test here
+    });
+});
