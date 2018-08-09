181102477c2e96bc45349671c72e5be33faf3bdb
michalpiatkowski <michal5piatkowski@gmail.com>
2018-08-09 21:52:52 +0200

    Added test for button onClick

--- frontend/src/components/LinkButton.test.js
+++ frontend/src/components/LinkButton.test.js
@@ -3,9 +3,17 @@ import LinkButton from './LinkButton';
 test('LinkButton', () => {
     it('should render label', () => {
         const label = 'test';
-    const buttonLink = shallow(<LinkButton label={label}/>);
+        const buttonLink = shallow(<LinkButton label={label}/>);
 
     expect(buttonLink.text()).toEqual(label);
 
     }) 
+
+    it('should render', () => {
+        const mockCallBack = jest.fn();
+        const buttonLink = shallow(<button onClick={mockCallBack}></button>);
+
+        buttonLink.find('button').simulate('click');
+        expect(mockCallBack.mock.calls.length).toEqual(1);
+    })
 })
