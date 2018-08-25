50222ecdd51d11937004db6e9e4b179a885a16ba
Dawid Szlachta <dawids@telestream.net>
2018-08-25 12:22:59 +0200

    Added react-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import store from './store';
 import App from './App';
@@ -11,7 +12,9 @@ import './styles/index.css';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router>
+            <Route path="/" component={App} />
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
