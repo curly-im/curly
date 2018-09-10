d87bebdc59a926ec80775fa247232f366d2dd583
Dawid Szlachta <dawids@telestream.net>
2018-09-10 15:21:37 +0200

    Fixed test format and Enzyme configuration
    It appears that Jest version we are using is not reporting bad test format
    (a TDD test with nested 'it's), but just asserts that test passed

--- frontend/src/components/UnreadMessagesIndicator.test.js
+++ frontend/src/components/UnreadMessagesIndicator.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import UnreadMessagesIndicator from './UnreadMessagesIndicator';
 
-test('UnreadMessagesIndicator', () => {
+Enzyme.configure({ adapter: new Adapter() });
+
+const { shallow } = Enzyme;
+
+describe('UnreadMessagesIndicator', () => {
     it('should check if component shows when count is greater than zero', () => {
         const unreadMessagesIndicator = shallow(<UnreadMessagesIndicator count={1} />);
         expect(unreadMessagesIndicator.find('div').hasClass('hidden')).toEqual(false);
         });
-    
+
     it('should show unread messages count', () => {
         const expectedMessagesCount = {count: 1}
         const unreadMessagesIndicator = shallow(<UnreadMessagesIndicator count={count} />);
@@ -20,6 +24,6 @@ test('UnreadMessagesIndicator', () => {
         const unreadMessagesIndicator = shallow(<UnreadMessagesIndicator count={0} />);
         expect(unreadMessagesIndicator.find('div').hasClass('hidden')).toEqual(true);
         });
-    
+
     }
 )
