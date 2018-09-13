6cb6813c181438e93a75206c65b3f325c2af33ed
michalpiatkowski <michal5piatkowski@gmail.com>
2018-09-13 11:27:07 +0200

    Added UnreadMessagesIndicator.css

--- frontend/src/components/UnreadMessagesIndicator.js
+++ frontend/src/components/UnreadMessagesIndicator.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import '../styles/UnreadMessagesIndicator.css'
+
 export default function UnreadMessagesIndicator({count}) {
 
     let hiddenClass = count <= 0 ? 'hidden' : '';
 
       return (
-        <div className={hiddenClass}>
-            {count}
+        <div className={hiddenClass} className='unread-messages-counter'>
+            <span className='unread-messages-count'>   
+                {count}
+            </span>
         </div>
       )}
 
