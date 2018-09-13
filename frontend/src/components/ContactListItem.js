04923b5c89039cf0ee4157412bfb021fcddddb1f
michalpiatkowski <michal5piatkowski@gmail.com>
2018-09-13 10:46:57 +0200

    Added UnreadMessagesIndicator component to ContactListItem

--- frontend/src/components/ContactListItem.js
+++ frontend/src/components/ContactListItem.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import UnreadMessageIndicator from './UnreadMessagesIndicator'
+
 export default function ContactListItem({ contact, onClick }) {
     return (
         <li onClick={() => onClick(contact)}>
           {contact.name}
+          <UnreadMessageIndicator count={1}/>
         </li>
     );
 }
