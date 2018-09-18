e9cefc799002fca32cc739129ed7bb0c17895bae
michalpiatkowski <michal5piatkowski@gmail.com>
2018-09-18 11:54:52 +0200

    Modified counter in ContactListItem

--- frontend/src/components/ContactListItem.js
+++ frontend/src/components/ContactListItem.js
@@ -3,10 +3,13 @@ import React from 'react';
 import UnreadMessageIndicator from './UnreadMessagesIndicator'
 
 export default function ContactListItem({ contact, onClick, count }) {
+    console.log(count);
+    console.log(contact);
     return (
         <li onClick={() => onClick(contact)}>
           {contact.name}
-          <UnreadMessageIndicator count={count}/>
+          <UnreadMessageIndicator count={count.get(contact.uuid)}/>
         </li>
     );
 }
+
